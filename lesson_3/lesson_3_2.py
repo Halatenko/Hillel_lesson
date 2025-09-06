9f3cf6322a4104cf5ba54e6807cb9126676bc029
Halatenko <galatenko2015@ukr.net>
2025-09-06 15:54:10 +0300

    add file

--- lesson_3/lesson_3_2.py
+++ lesson_3/lesson_3_2.py
@@ -1,5 +1,7 @@
-list = [1, 'F', 2, 3, 4, 5]
-
-move_element = list.pop(-1)
-list.insert(0, move_element)
-print(list)
+list3 = [1, 'F', 2, 3, 4, 5]
+if list3 == []:
+    print(list3)
+else:
+    move_element = list3.pop()
+    list3.insert(0, move_element)
+    print(list3)
